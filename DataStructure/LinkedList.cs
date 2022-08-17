ed2a0559509b2108cdf3125c6b2fc318231fc788
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-17 19:12:21 +0530

    first

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        Node head;
+
+        public void AddNode(int data)
+        {
+
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into linkedList", node.data);
+        }
+
+        public void Display()
+        {
+            if(head == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The list Contains: ");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Linked List Contains : ");
+                Console.ResetColor();
+                Node tempnode = head;
+                while( tempnode != null )
+                {
+                    Console.WriteLine(tempnode.data + " ");
+                    tempnode = tempnode.Next;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructure
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList l1 = new LinkedList();
+            l1.AddNode(56);
+            l1.AddNode(30);
+            l1.AddNode(70);
+            l1.Display();
+        }
+
+    }
+}
